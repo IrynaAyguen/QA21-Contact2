10bd91779f1a42fcfd12ae19fccd837793e10c64
IrynaAyguen <88163152+IrynaAyguen@users.noreply.github.com>
2021-10-28 16:59:04 +0200

    komments in RemoveContactTests

--- src/test/java/com/telran/contact/RemoveContactTests.java
+++ src/test/java/com/telran/contact/RemoveContactTests.java
@@ -7,36 +7,31 @@ import org.testng.annotations.Test;
 
 public class RemoveContactTests extends TestBase {
 
-
     @BeforeMethod
     public void ensurePreconditions() {
         if (!isLoginTabPresent()) {
             // click on Logout button
             clickOnSignOutButton();
         }
+        clickOnLoginTab();
+        login(new User()
+                .setEmail("ira@web.de")
+                .setPassword("Ira123123_"));
 
-            clickOnLoginTab();
-            login(new User()
-                    .setEmail("ira@web.de")
-                    .setPassword("Ira123123_"));
-
-           int i = (int) ((System.currentTimeMillis()) / 1000) % 3600; // return 4 nubmer
-            addNewContact("Karl", "Adam", "3333333", "adam" + i + "@dm.com", "Koblenz", "torwart");
-
-        }
-
+        int i = (int) ((System.currentTimeMillis()) / 1000) % 3600; // return 4 nubmer
+        addNewContact("Karl", "Adam", "3333333", "adam" + i + "@dm.com", "Koblenz", "torwart");
+    }
 
     @Test
-
     public void removeContactTest() throws InterruptedException {
         int sizeBefore = sizeOfContacts();
-        System.out.println(sizeBefore);
+        System.out.println(sizeBefore);  //for control
         removeContact();
         Thread.sleep(1000);
         int sizeAfter = sizeOfContacts();
-        System.out.println(sizeAfter);
+        System.out.println(sizeAfter);   //for control
 
-        Assert.assertEquals(sizeBefore, sizeAfter+1);
+        Assert.assertEquals(sizeBefore, sizeAfter + 1);
     }
 
 }
