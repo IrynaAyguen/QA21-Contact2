d73d46743213355e807205a06677e2e3ae448363
IrynaAyguen <88163152+IrynaAyguen@users.noreply.github.com>
2021-11-08 17:22:20 +0100

    for Forefox: ApplManag,TestBase

--- src/test/java/com/telran/contact/fw/ApplicationManager.java
+++ src/test/java/com/telran/contact/fw/ApplicationManager.java
@@ -3,6 +3,8 @@ package com.telran.contact.fw;
 import com.telran.contact.tests.TestBase;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.slf4j.Logger;
@@ -15,11 +17,16 @@ public class ApplicationManager {
 
     //static WebDriver driver;
     static EventFiringWebDriver driver;
+    String browser;
 
     UserHelper user;         // declaration of Helpers
     ContactHelper contact;
     HomeHelper home;
     HeaderHelper header;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public static class MyListener extends AbstractWebDriverEventListener {
 
@@ -28,13 +35,13 @@ public class ApplicationManager {
         @Override
         public void beforeFindBy(By by, WebElement element, WebDriver driver) {
 
-            logger.info("Start search "+by);
+            logger.info("Start search " + by);
         }
 
         @Override
         public void afterFindBy(By by, WebElement element, WebDriver driver) {
 
-            logger.info(by+ " found");
+            logger.info(by + " found");
         }
 
         @Override
@@ -63,7 +70,13 @@ public class ApplicationManager {
         //driver = new FirefoxDriver();
         //driver = new ChromeDriver();
         //driver = new EdgeDriver();
-        driver= new EventFiringWebDriver(new ChromeDriver());
+        if (browser.equals(BrowserType.CHROME)) {
+            driver = new EventFiringWebDriver(new ChromeDriver());
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            driver = new EventFiringWebDriver(new FirefoxDriver());
+        }
+        //driver= new EventFiringWebDriver(new ChromeDriver());
+
         driver.get("https://contacts-app.tobbymarshall815.vercel.app");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
